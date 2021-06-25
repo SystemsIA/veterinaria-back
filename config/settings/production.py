867dfe861cdfa864b78e6f2fb6c8245634ee2507
Lherner Remón <lherner.remon.27@unsch.edu.pe>
2021-06-24 19:08:10 -0500

    fix: correcion allowed-hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import env
 
 # GENERAL
 SECRET_KEY = env("DJANGO_SECRET_KEY")
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["api.rrqq.me"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["gaa.rrqq.me"])
 
 # DATABASES
 DATABASES = {"default": env.db("DATABASE_URL", default="sqlite:///db.sqlite3")}  # noqa F405
