6c020b97458484eed1fa7b1dd37e68064067dbac
Lherner Remon <lherner21@gmail.com>
2021-12-28 14:45:07 -0500

    fix: probando acceso privado aws

--- veterinaria_api/utils/storages.py
+++ veterinaria_api/utils/storages.py
@@ -8,6 +8,12 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = "media"
-    default_acl = "private"
     file_overwrite = False
+    # Tipo de almacenamiento
+    default_acl = "private"
+    # Campo necesario
     custom_domain = False
+    # peticiones por authenticación
+    querystring_auth = True
+    # Tiempo de vida del archivo (segundos)
+    querystring_expire = 60 * 60
